8418728afc66aa7dc89f3258e290f6de51b2df74
Techno-k <backupkislay@gmail.com>
2025-11-12 16:10:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Smooth scroll to Team section
+document.querySelector('.cta-btn').addEventListener('click', () => {
+  document.querySelector('.team').scrollIntoView({ behavior: 'smooth' });
+});
+
